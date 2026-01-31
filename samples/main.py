e92aedba6efe0095078452dc830e1e80cd2ee23d
Manohar gunturu <gunturumanohar2@gmail.com>
2026-01-30 18:39:56 -0800

    update samples

--- samples/main.py
+++ samples/main.py
@@ -1,6 +1,6 @@
 from python.RAG.ChatOllamaRAGPipeline import ChatOllamaRAGPipeline
 
-rag = ChatOllamaRAGPipeline()
+rag = ChatOllamaRAGPipeline(chunk_size=1024, chunk_overlap=100, model="llama3")
 rag.build_index(["mahu_balance.txt", "dosa_recipe.txt"])
-answer  = rag.answer("To make dosa batter with 1 and half cup of lentils, can you give me the recipe?")
+answer  = rag.answer("How much balance Mahu has?")
 print(answer)
